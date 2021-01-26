75251e9aa39bc6a7d0c1e032ec4723a068436334
Crowdbotics <team@crowdbotics.com>
2021-01-26 11:50:17 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn23197030', human_name: 'Sign In 2 3', access_route: 'SignIn23197030'},
 { name: 'BlankScreen1197010', human_name: 'Blank Screen 1', access_route: 'BlankScreen1197010'},
 { name: 'BlankScreen0196996', human_name: 'Blank Screen 0', access_route: 'BlankScreen0196996'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn23197030Navigator from '../features/SignIn23197030/navigator';
 import BlankScreen1197010Navigator from '../features/BlankScreen1197010/navigator';
 import BlankScreen0196996Navigator from '../features/BlankScreen0196996/navigator';
 
@@ -15,6 +16,7 @@ import BlankScreen0196996Navigator from '../features/BlankScreen0196996/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn23197030: { screen: SignIn23197030Navigator },
 BlankScreen1197010: { screen: BlankScreen1197010Navigator },
 BlankScreen0196996: { screen: BlankScreen0196996Navigator },
 
